07e33c9799b6bb6952b93fcf78439a9cd8d80618
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-04-20 00:14:23 +0500

    worldwise

---
+++ worldwise/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import Notfound from "./pages/Notfound";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+const Homepage = () => {
+  return (
+    <div>
+      <PageNav />
+    </div>
+  );
+};
+
+export default Homepage;
